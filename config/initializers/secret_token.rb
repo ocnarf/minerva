86e1b16e765e2df54207c5c055d5b9b785354b0b
Franco <andoni.f@gmail.com>
2013-10-29 15:49:56 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MinervaSample::Application.config.secret_key_base = '5cb360ccfc603341921898849654f29c11d34ff825bf4f1a8aebbaa387f33ff5e57a996dd81fd85423298406a1e78987d933a267904538143dbfcedceb0a4c0a'
