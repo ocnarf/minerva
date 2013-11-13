e63f48013a7e19bde3ee7ed9a7edb7a3df70dda9
Franco <andoni.f@gmail.com>
2013-11-13 00:53:55 -0800

    subscribe to pubsub

--- app/controllers/pub_sub_controller.rb
+++ app/controllers/pub_sub_controller.rb
@@ -1,4 +1,23 @@
 class PubSubController < ApplicationController
-  def callback
+	after_filter do
+    puts response.body
+	end
+  
+  def subscribe 
+  	puts "subscribing"
+  	pshb = SuperfeedrPshb::SuperfeedrPshb.new("minerva", "soymexicano", 
+  		"http://67.180.177.165")
+  	pshb.subscribe("/pub_sub/callback", "http://push-pub.appspot.com/feed", "superfeedtest")
   end
+
+  def callback
+  	 if !params["hub.challenge"].nil?
+       @challenge = params["hub.challenge"]
+       render :action => "callback", :status => :ok, :layout => false
+   	 else
+  	   @challenge = "uhoh!"
+       render :action => "callback", :status => 404
+  	 end
+  end
+
 end
