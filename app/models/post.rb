edf4b68895dac6ef3657940bf41a58dcda88f03f
Franco <andoni.f@gmail.com>
2013-11-21 20:40:33 -0800

    get redirected url for new posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,9 +15,12 @@ class Post < ActiveRecord::Base
 
   feed.entries.each do |item|
 
-    url = item.url
+    # follow url redirection to get final location of entry
+    response = HTTParty.get(item.url)
+    redirected_url = response.request.last_uri.to_s
+
     published = item.published.to_datetime
-    host = URI.parse(url).host
+    host = URI.parse(redirected_url).host
 
     if !Site.exists?( :url => host)
       Site.create( {:url => host} )
@@ -25,7 +28,7 @@ class Post < ActiveRecord::Base
 
     site_id = Site.where( :url => host ).first.id
 
-    Post.create( { :url => url, :published => published, :site_id => site_id } )
+    Post.create( { :url => redirected_url, :published => published, :site_id => site_id } )
   end
  end
 
